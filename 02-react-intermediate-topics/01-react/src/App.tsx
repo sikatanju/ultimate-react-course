979e6cdec4b8b4d0bdeceaf67f4de629540d9c15
sikatanju <saadtanju@gmail.com>
2024-11-04 08:13:22 +0600

    Completed Queries & started mutation.

---
+++ 02-react-intermediate-topics/01-react/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import PostList from "./react-query/PostList";
+import TodoForm from "./react-query/TodoForm";
+import TodoList from "./react-query/TodoList";
+
+function App() {
+  return (
+    <>
+      <TodoForm />
+      {/* <PostList /> */}
+      <TodoList />;
+    </>
+  );
+}
+
+export default App;
